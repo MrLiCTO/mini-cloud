28f0100bee87756c9281abfae861367be943e318
MrLiCTO <javaeemrli@163.com>
2017-05-22 07:11:20 +0800

    简单的整合jpa

--- balance/src/test/java/com/slli/cloud/balance/BalanceApplicationTests.java
+++ balance/src/test/java/com/slli/cloud/balance/BalanceApplicationTests.java
@@ -2,15 +2,25 @@ package com.slli.cloud.balance;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BalanceApplicationTests {
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
 
-	@Test
+	@Test//http://www.tuicool.com/articles/AvUnE3J
 	public void contextLoads() {
+		rabbitTemplate.setConfirmCallback((correlationData,ack,cause)->{
+
+		});
 	}
 
 }
