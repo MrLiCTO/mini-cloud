a70fac8e29d826e34369b31124c89a415deaa1b0
MrLiCTO <javaeemrli@163.com>
2017-05-22 17:40:09 +0800

    改了个bug

--- ui/src/main/java/com/slli/cloud/ui/UiApplication.java
+++ ui/src/main/java/com/slli/cloud/ui/UiApplication.java
@@ -2,10 +2,10 @@ package com.slli.cloud.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UiApplication {
 
 	public static void main(String[] args) {
