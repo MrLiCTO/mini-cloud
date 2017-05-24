a78a39dc905133778670c4789a9c9572b4d84a20
MrLiCTO <javaeemrli@163.com>
2017-05-24 14:33:50 +0800

    rabbitMQ

--- pay/src/test/java/com/slli/cloud/pay/PayApplicationTests.java
+++ pay/src/test/java/com/slli/cloud/pay/PayApplicationTests.java
@@ -2,6 +2,8 @@ package com.slli.cloud.pay;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PayApplicationTests {
 
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
 	@Test
+	//http://www.tuicool.com/articles/AvUnE3J
+	//http://blog.csdn.net/mra__s__/article/details/55011530
 	public void contextLoads() {
+		rabbitTemplate.convertAndSend("pay","abcdefg");
 	}
 
 }
